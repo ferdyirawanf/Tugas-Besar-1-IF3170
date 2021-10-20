eee599b1d6ade58198896f7d46c3ca5d939e8137
frnzoshi-tech <75726616+frnzoshi-tech@users.noreply.github.com>
2021-10-20 19:57:45 +0700

    update dumper filename

--- dumper.py
+++ dumper.py
@@ -2,14 +2,15 @@ from src.ai import *
 from src.constant import Path
 from src.utility import dump
 
-def dumper(path_b1, path_b2, path_pvb):
-    model = Minimax2()
-    dump(model, Path.BVB_P1.format(path_b1))
-    dump(model, Path.BVB_P2.format(path_b2))
-    dump(model, Path.PVB.format(path_pvb))
+def dumper(path_b1, path_b2):
+    model1 = MinimaxGroup16()
+    model2 = LocalSearchGroup16()
+    dump(model1, Path.BVB_P1.format(path_b1))
+    dump(model2, Path.BVB_P2.format(path_b2))
+    dump(model1, Path.PVB.format(path_b1))
+    dump(model2, Path.PVB.format(path_b2))
 
 if __name__ == '__main__':
-    bot1_filename = 'random-1.pkl' 
-    bot2_filename = 'random-2.pkl'
-    pvp_bot_filename = 'random-1.pkl'
-    dumper(bot1_filename, bot2_filename, pvp_bot_filename)
+    bot1_filename = 'AiTB_minimax.pkl' 
+    bot2_filename = 'AiTB_local_search.pkl'
+    dumper(bot1_filename, bot2_filename)
