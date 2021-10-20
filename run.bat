8312ae3656767161f69ba524c5d9c0a297ea0f40
frnzoshi-tech <75726616+frnzoshi-tech@users.noreply.github.com>
2021-10-20 22:26:00 +0700

    update bvb - local vs minimax

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-python main.py --is_dump --row 6 --col 7 --type pvb --player_choice 0 --thinking_time 3 --bot1 AiTB_minimax.pkl --bot2 AiTB_local_search.pkl
+python main.py --is_dump --row 6 --col 7 --type bvb --thinking_time 3 --bot1 AiTB_local_search.pkl --bot2 AiTB_minimax.pkl
