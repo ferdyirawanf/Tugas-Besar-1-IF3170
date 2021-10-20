381cf708ead3072143d262fb9819137d4173b3f2
ridhodaffasyah <ridhodaffasyah01@gmail.com>
2021-10-20 14:44:47 +0700

    fix minimax and thinking_time

--- src/ai/minimax1.py
+++ src/ai/minimax1.py
@@ -1,24 +1,18 @@
 import math
 import random
 from time import time
-from src.model import Player
-from src.model.player import Player
 
 from src.constant import ColorConstant, GameConstant, ShapeConstant
-from src.model.board import Board
-from src.model.state import State
-from src.model.piece import Piece
-from src.utility import is_win, is_full, place
+from src.model import Board, State
+from src.utility import is_win, is_full
 import copy
-
-from typing import Tuple
 
 class BotMinimaxAB:
     
     def __init__(self):
         pass
     
-    def node(self, board: Board, n_player):
+    def node(self, state: State, board: Board, n_player):
         node = []
         
         if n_player == 0:
@@ -27,7 +21,7 @@ class BotMinimaxAB:
         else:
             player_color = GameConstant.PLAYER2_COLOR
             player_shape = GameConstant.PLAYER2_SHAPE
-
+        
         for i in range(board.col):
             if (board[0, i].shape == ShapeConstant.BLANK):
                 copy_board = copy.deepcopy(board)
@@ -37,6 +31,9 @@ class BotMinimaxAB:
                 copy_board[row, i].color = player_color
                 copy_board[row, i].shape = player_shape
                 node.append((i,copy_board))
+            else :
+                pass
+            
         return node
                 
     def heuristicAB(self, board: Board, n_player: int) :
@@ -102,6 +99,22 @@ class BotMinimaxAB:
                         value = value - 10**3
                         shape_value = shape
                     if (board[row, col].shape != shape and board[row, col+1].shape != shape and board[row, col+2].shape != shape and board[row, col+3].shape != shape and board[row, col+1].shape != ShapeConstant.BLANK and board[row, col+2].shape != ShapeConstant.BLANK and board[row, col+3].shape != ShapeConstant.BLANK and board[row, col].shape != ShapeConstant.BLANK): 
+                        value = value - 10**3
+                        shape_value = shape
+                    
+                    #Maximazing 4 streak with hole in the middle
+                    if (board[row, col].color == color and board[row, col+1].color == color and board[row, col+2].color == color and board[row, col+3].color == color and ((board[row, col+1].color == ColorConstant.BLACK and board[row, col+2].color != ColorConstant.BLACK) or (board[row, col+1].color != ColorConstant.BLACK and board[row, col+2].color == ColorConstant.BLACK)) and board[row, col+3].color != ColorConstant.BLACK and board[row, col].color != ColorConstant.BLACK):
+                        value = value + 10**3
+                        shape_value = shape
+                    if (board[row, col].shape == shape and board[row, col+1].shape == shape and board[row, col+2].shape == shape and board[row, col+3].shape == shape and ((board[row, col+1].shape == ShapeConstant.BLANK and board[row, col+2].shape != ShapeConstant.BLANK) or (board[row, col+1].shape != ShapeConstant.BLANK and board[row, col+2].shape == ShapeConstant.BLANK)) and board[row, col+3].shape != ShapeConstant.BLANK and board[row, col].shape != ShapeConstant.BLANK): 
+                        value = value + 10**3
+                        shape_value = shape_opp
+                        
+                    #Minimazing 4 streak
+                    if (board[row, col].color != color and board[row, col+1].color != color and board[row, col+2].color != color and board[row, col+3].color != color and ((board[row, col+1].color == ColorConstant.BLACK and board[row, col+2].color != ColorConstant.BLACK) or (board[row, col+1].color != ColorConstant.BLACK and board[row, col+2].color == ColorConstant.BLACK)) and board[row, col+3].color != ColorConstant.BLACK and board[row, col].color != ColorConstant.BLACK):
+                        value = value - 10**3
+                        shape_value = shape
+                    if (board[row, col].shape != shape and board[row, col+1].shape != shape and board[row, col+2].shape != shape and board[row, col+3].shape != shape and ((board[row, col+1].shape == ShapeConstant.BLANK and board[row, col+2].shape != ShapeConstant.BLANK) or (board[row, col+1].shape != ShapeConstant.BLANK and board[row, col+2].shape == ShapeConstant.BLANK)) and board[row, col+3].shape != ShapeConstant.BLANK and board[row, col].shape != ShapeConstant.BLANK): 
                         value = value - 10**3
                         shape_value = shape
                         
@@ -198,7 +211,7 @@ class BotMinimaxAB:
                     #Maximazing 4 streak
                     if (board[row, col].color == color and board[row-1, col+1].color == color and board[row-2, col+2].color == color and board[row-3, col+3].color == color and board[row-1,col+1].color != ColorConstant.BLACK and board[row-2, col+2].color != ColorConstant.BLACK and board[row-3,col+3].color != ColorConstant.BLACK and board[row, col].color != ColorConstant.BLACK):
                         value = value + 10**3
-                        shape_value = shape
+                        shape_value = shape_opp
                     if (board[row, col].shape == shape and board[row-1, col+1].shape == shape and board[row-2, col+2].shape == shape and board[row-3, col+3].shape == shape and board[row-1, col+1].shape != ShapeConstant.BLANK and board[row-2, col+2].shape != ShapeConstant.BLANK and board[row-3,col+3].shape != ShapeConstant.BLANK and board[row, col].shape != ShapeConstant.BLANK): 
                         value = value + 10**3
                         shape_value = shape_opp
@@ -266,24 +279,43 @@ class BotMinimaxAB:
                         
                 except IndexError :
                     pass
-        
+                
         return value, shape_value
     
     def find(self, state, n_player, thinking_time) :
-        value, col, shape = self.MinimaxAB(state, state.board, n_player, 5, -math.inf, math.inf, thinking_time)
+        start = time()
+        value, col, shape = self.MinimaxAB(state, state.board, n_player, 3, -math.inf, math.inf, thinking_time, start)
+        listShape = []
+        for k, v in state.players[n_player].quota.items():
+            listShape += list([[k,v]])
+
+        for i in range (len(listShape[0])):
+            if listShape[i][0] == GameConstant.PLAYER1_SHAPE and listShape[i][1] == 0:
+                shape = GameConstant.PLAYER2_SHAPE
+            elif listShape[i][0] == GameConstant.PLAYER2_SHAPE and listShape[i][1] == 0:
+                shape = GameConstant.PLAYER1_SHAPE
         print(value, col, shape)
         return col, shape 
         
-    def MinimaxAB(self, state, board: Board, n_player: int, depth: int, alpha: int, beta: int, thinking_time) :
-        start = time()
-
-        col_move = random.randint(0, board.col-1)
-        nodes = self.node(board, n_player)
-
-        if n_player == 0:
+    def MinimaxAB(self, state, board: Board, n_player: int, depth: int, alpha: int, beta: int, thinking_time, start) :
+        
+        valid_col = []
+        for i in range(board.col):
+            if (board[0,i].shape == ShapeConstant.BLANK):
+                valid_col += [i]
+                
+        col_move = random.choice(valid_col)
+        
+        if (n_player==0):
             shape = GameConstant.PLAYER1_SHAPE
         else :
             shape = GameConstant.PLAYER2_SHAPE
+            
+        now = time()
+        if thinking_time <= (now-start):
+            return 0, col_move, shape
+        
+        nodes = self.node(state, board, n_player)
 
         is_terminal_state = False
         if is_win(board) != None or is_full(board) :
@@ -293,42 +325,12 @@ class BotMinimaxAB:
         if is_terminal_state:
             if (n_player==0) :
                 value, shape = self.heuristicAB(board, n_player)
-                listShape = []
-                player = (state.round - 1) % 2
-                for k, v in state.players[player].quota.items():
-                    listShape += list([[k,v]])
-
-                for i in range (len(listShape[0])):
-                    if listShape[i][0] == GameConstant.PLAYER1_SHAPE and listShape[i][1] == 0:
-                        shape = GameConstant.PLAYER2_SHAPE
-                    elif listShape[i][0] == GameConstant.PLAYER2_SHAPE and listShape[i][1] == 0:
-                        shape = GameConstant.PLAYER1_SHAPE
                 return -math.inf, col_move, shape
             else :
                 value, shape = self.heuristicAB(board, n_player)
-                listShape = []
-                player = (state.round - 1) % 2
-                for k, v in state.players[player].quota.items():
-                    listShape += list([[k,v]])
-
-                for i in range (len(listShape[0])):
-                    if listShape[i][0] == GameConstant.PLAYER1_SHAPE and listShape[i][1] == 0:
-                        shape = GameConstant.PLAYER2_SHAPE
-                    elif listShape[i][0] == GameConstant.PLAYER2_SHAPE and listShape[i][1] == 0:
-                        shape = GameConstant.PLAYER1_SHAPE
                 return math.inf, col_move, shape
         elif depth == 0:
             value, shape = self.heuristicAB(board, n_player)
-            listShape = []
-            player = (state.round - 1) % 2
-            for k, v in state.players[player].quota.items():
-                listShape += list([[k,v]])
-
-            for i in range (len(listShape[0])):
-                if listShape[i][0] == GameConstant.PLAYER1_SHAPE and listShape[i][1] == 0:
-                    shape = GameConstant.PLAYER2_SHAPE
-                elif listShape[i][0] == GameConstant.PLAYER2_SHAPE and listShape[i][1] == 0:
-                    shape = GameConstant.PLAYER1_SHAPE
             return value, col_move, shape
 
         #Rekurens
@@ -337,13 +339,7 @@ class BotMinimaxAB:
             for node in nodes:
                 col, new_board = node
 
-                now = time()
-                if thinking_time <= (now - start) :
-                    print("MASUK PAK eko 1")
-                    print(now - start)
-                    break
-
-                tempMove = self.MinimaxAB(state, new_board, n_player+1, depth-1, alpha, beta, thinking_time)
+                tempMove = self.MinimaxAB(state, new_board, n_player+1, depth-1, alpha, beta, thinking_time, start)
 
                 if tempMove[0] > value :
                     value = tempMove[0]
@@ -361,13 +357,7 @@ class BotMinimaxAB:
             for node in nodes:
                 col, new_board = node
 
-                now = time()
-                if thinking_time <= (now - start) :
-                    print("MASUK PAK eko 2")
-                    print(now - start)
-                    break
-
-                tempMove = self.MinimaxAB(state, new_board, n_player-1, depth-1, alpha, beta, thinking_time)
+                tempMove = self.MinimaxAB(state, new_board, n_player-1, depth-1, alpha, beta, thinking_time, start)
 
                 if tempMove[0] < value:
                     value = tempMove[0]
@@ -379,8 +369,8 @@ class BotMinimaxAB:
                     break
             
             return value, col_move, shape
-               
-#BotMinimaxAB.find(BotMinimaxAB(), Board(6,7), 0, 5, -math.inf, math.inf)
+
+#BotMinimaxAB.find(BotMinimaxAB(), State(Board(6,7),[0,1],1), 0, 100)
 #BotMinimaxAB.node(BotMinimaxAB(Board(6,7),0), Board(6,7), 0);
 #BotMinimaxAB.MinimaxAB(BotMinimaxAB(Board(6,7),0), Board(6,7), State(Board(6,7), [0,1], 1), 0, 5, -math.inf, math.inf, True, 10.0)
-#BotMinimaxAB.heuristicAB(BotMinimaxAB(Board(6,7),0), Board(6,7), 0)
+#BotMinimaxAB.heuristicAB(BotMinimaxAB(), Board(6,7), 0)
